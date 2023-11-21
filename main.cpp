20674b816a11307c207e4ec6bf78e0c4f92fba1b
Aleksander Kosarskii <kocagg@mail.ru>
2023-11-21 23:44:19 +0300

    refactor : now particle is a class

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 
 
-struct Particle {
-    double x, y, z;
-    bool stuck;
+class Particle {
+public:
+    Particle(double x, double y, double z, double velocity)
+            : x_(x), y_(y), z_(z), velocity_(velocity) {}
 
-    Particle(double x, double y, double z) : x(x), y(y), z(z), stuck(false) {}
+    void move(double dt) {
+        x_ += velocity_ * dt;
+        y_ += velocity_ * dt;
+        z_ += velocity_ * dt;
+    }
+
+    bool checkCollision(double surfaceZ) const {
+        return z_ <= surfaceZ;
+    }
+
+    double getX() const { return x_; }
+    double getY() const { return y_; }
+    double getZ() const { return z_; }
+
+private:
+    double x_;
+    double y_;
+    double z_;
+
+    double velocity_;
 };
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
