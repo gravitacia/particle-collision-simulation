14fce9c4c3a3e7e47a6c6e251d303c4e557f42df
Aleksander Kosarskii <kocagg@mail.ru>
2023-11-21 23:13:41 +0300

    feat : add particle struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+
+struct Particle {
+    double x, y, z;
+    bool stuck;
+
+    Particle(double x, double y, double z) : x(x), y(y), z(z), stuck(false) {}
+};
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
